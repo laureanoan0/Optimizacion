75f2d9937e5a8be2d23e1df810d63e7106d7b93d
oliveriofogiel <oliifogiel@gmail.com>
2026-07-23 11:11:06 -0300

    Fixed UIManager Errors

--- Assets/Scripts/UI/UiManager.cs
+++ Assets/Scripts/UI/UiManager.cs
@@ -3,7 +3,7 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public enum Scenes { MainMenu, GameplayScene, FinalScreen}
+public enum Scenes { MainMenu, GameplayScene, FinalScreen }
 
 public class UiManager : MonoBehaviour
 {
@@ -17,28 +17,26 @@ public class UiManager : MonoBehaviour
     [Header("Main Menu Scene")]
     [SerializeField] private Button playButton;
     [SerializeField] private Button quitButton;
-    [SerializeField] private string gameplaySceneName = "GameplayScene"; 
+    [SerializeField] private string gameplaySceneName = "GameplayScene";
 
     [Header("Final Scene")]
     [SerializeField] private Button replayButton;
     [SerializeField] private Button mainMenuButton;
     [SerializeField] private string menuSceneName = "MainMenuScene";
-    
 
     private ScoreManager scoreManager;
-    private AsyncOperation preloadGameplayScene;
-    private AsyncOperation preloadMainMenuScene;
 
     private void Awake()
     {
-        if(CurrentScene == Scenes.MainMenu)
+        if (CurrentScene == Scenes.MainMenu)
         {
             InitializeMainMenu();
         }
-        else if(CurrentScene == Scenes.GameplayScene) 
+        else if (CurrentScene == Scenes.GameplayScene)
         {
             InitializeScoreDisplay();
-        }else
+        }
+        else
         {
             InitializeFinalScreen();
         }
@@ -50,49 +48,38 @@ public class UiManager : MonoBehaviour
         scoreManager.OnScoreChanged += UpdateScoreText;
         UpdateScoreText(scoreManager.Score);
     }
+
     private void InitializeMainMenu()
     {
-        Debug.Log("HOLA");
+        Debug.Log("HOLA - estoy en MainMenu");
         playButton.onClick.AddListener(OnPlayClicked);
         quitButton.onClick.AddListener(OnQuitClicked);
-
-        preloadGameplayScene = SceneManager.LoadSceneAsync(gameplaySceneName);
-        preloadGameplayScene.allowSceneActivation = false;
     }
+
     private void InitializeFinalScreen()
     {
+        Debug.Log("HOLA - estoy en FinalScreen");
         replayButton.onClick.AddListener(OnPlayClicked);
         mainMenuButton.onClick.AddListener(OnMenuClicked);
-
-        preloadGameplayScene = SceneManager.LoadSceneAsync(gameplaySceneName);
-        preloadGameplayScene.allowSceneActivation = false;
-
-        preloadMainMenuScene = SceneManager.LoadSceneAsync(menuSceneName);
-        preloadMainMenuScene.allowSceneActivation = false;
     }
 
     private void OnQuitClicked()
     {
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false; 
+        UnityEditor.EditorApplication.isPlaying = false;
 #else
-        Application.Quit();  
+        Application.Quit();
 #endif
     }
+
     private void OnPlayClicked()
     {
-        if(preloadGameplayScene != null)
-        {
-            Debug.Log("CHau");
-           // preloadGameplayScene.allowSceneActivation = true;
-        }
+        SceneManager.LoadScene(gameplaySceneName);
     }
+
     private void OnMenuClicked()
     {
-        if (preloadMainMenuScene != null)
-        {
-            preloadMainMenuScene.allowSceneActivation = true;
-        }
+        SceneManager.LoadScene(menuSceneName);
     }
 
     private void OnDisable()
@@ -102,6 +89,7 @@ public class UiManager : MonoBehaviour
             scoreManager.OnScoreChanged -= UpdateScoreText;
         }
     }
+
     private void UpdateScoreText(int newScore)
     {
         scoreText.text = prefix + newScore;
